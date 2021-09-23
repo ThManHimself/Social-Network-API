888e7012f3cfccbcc7e5b3f2ad876abcf5b8b8d4
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-23 11:57:20 -0500

    minor changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
-// add prefic of '/thoughts' to routes created in 'thought-routes.js'
+// add prefix of '/thoughts' to routes created in 'thought-routes.js' and 'reaction-routes.js'
 router.use('/thoughts', thoughtRoutes);
-// add prefic of '/users' to routes created in 'user-routes.js'
+// add prefix of '/users' to routes created in 'user-routes.js'
 router.use('/users', userRoutes);
 
 

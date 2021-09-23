6b12586ae266f59b25fe29011bcfdc053fdf54c7
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-23 13:21:14 -0500

    finiched user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,11 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    // friend controllers
+    addFriend,
+    deleteFriend,
+    getAllFriends
 } = require('../../controllers/user-controllers');
 
 // Set up GET all and POST -- /api/users --
@@ -15,10 +19,22 @@ router
 
 // Set up GET 1, PUT, and DELETE -- /api/users/:id --
 router
-    .route(':id')
+    .route('/:id')
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser);
+
+// Set up PUT and DELETE for friends -- /api/users/:userId/friends
+// userId = user you're adding a friend too
+// friendId = user you're adding to friends list
+router
+    .route('/:id/friends')
+    .get(getAllFriends)
+    .put(addFriend);
+
+router
+    .route('/:id/friends/:_id')
+    .delete(deleteFriend);
 
 // exporting... ... ...
 module.exports = router;
